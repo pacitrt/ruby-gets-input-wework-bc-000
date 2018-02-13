d3959bee69c1244548b52297eeab0c73b2b557bf
Rio Pacit <rio.pacit@wework.com>
2018-02-13 18:16:55 +0000

    automatically backed up by learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,3 +1,6 @@
+# ./lib/hello_ruby_programmer.rb:1:in `greeting'
+     # ./spec/lib/hello_ruby_programmer_spec.rb:7:in `block (2 levels) in <top (required)>'
+
 def greeting
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end

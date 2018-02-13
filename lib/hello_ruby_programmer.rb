37f774455d80c57af29673e20501bad63fe52ed0
Rio Pacit <rio.pacit@wework.com>
2018-02-13 18:20:46 +0000

    automatically backed up by learn

--- lib/hello_ruby_programmer.rb
+++ lib/hello_ruby_programmer.rb
@@ -1,7 +1,7 @@
+puts "Please enter your name so that we can greet you more personally:"
+name = gets.strip
 
-
-def greeting
+def greeting(name)
   puts "Hi, #{name}! Welcome to the wonderful world of Ruby programming."
 end
 
-puts greeting
